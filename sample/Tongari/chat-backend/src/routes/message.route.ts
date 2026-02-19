1354ef9af3456c1dd27b3b6568245d4f141577d3
Tongari-Boy <tongariboy@outlook.com>
2026-02-19 10:09:31 +0900

    履歴取得(今はメモリを仮DBとして用意)

--- sample/Tongari/chat-backend/src/routes/message.route.ts
+++ sample/Tongari/chat-backend/src/routes/message.route.ts
@@ -4,10 +4,14 @@ import {
   createAudioMessage,
   uploadMiddleware
 } from "../controllers/message.controller";
+import {getMessages} from "../controllers/message.controller";
 
 const router = Router();
 
 //POST/audioが来たらcontllerのcreateAudioMessageを呼び出す
 router.post("/audio", uploadMiddleware, createAudioMessage);
+
+//GET/messagesが来たらcontrollerのgetMessagesを呼び出す
+router.get("/messages", getMessages);
 
 export default router;
